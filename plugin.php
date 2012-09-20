90e2ccc6782e7759383d7b164de14d334a869420
Thomas Scholz <info@toscho.de>
2012-09-20 21:23:58 +0200

    Remove noise.

--- plugin.php
+++ plugin.php
@@ -1,5 +1,4 @@
 <?php # -*- coding: utf-8 -*-
-declare ( encoding = 'UTF-8' );
 /**
  * Plugin Name: T5 Opera Speed Dial Preview
  * Plugin URI:  https://github.com/toscho/T5-Opera-Speed-Dial-Preview
@@ -12,28 +11,7 @@ declare ( encoding = 'UTF-8' );
  * License URI: http://www.opensource.org/licenses/mit-license.php
  *
  * Copyright (c) 2012 Thomas Scholz
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
  */
-
-// Not a WordPress context? Stop.
-! defined( 'ABSPATH' ) and exit;
 
 // Wait until all needed functions are loaded.
 add_action( 'plugins_loaded', array ( 'T5_Opera_Speed_Dial', 'get_instance' ) );
